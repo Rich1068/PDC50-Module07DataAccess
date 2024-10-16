d99247d86806c0793431b1944aa904eb11b3e0ce
Richard Darwin Sy <rs106848@gmail.com>
2024-10-16 12:57:06 +0800

    Add project files.

---
+++ View/ViewPersonal.xaml.cs
@@ -0,0 +1,17 @@
+//added
+using Module07DataAccess.Services;
+using Module07DataAccess.ViewModel;
+
+namespace Module07DataAccess.View;
+
+public partial class ViewPersonal : ContentPage
+{
+	public ViewPersonal()
+	{
+		InitializeComponent();
+
+		var personalViewModel = new PersonalViewModel();
+		BindingContext = personalViewModel;
+	}
+
+}
