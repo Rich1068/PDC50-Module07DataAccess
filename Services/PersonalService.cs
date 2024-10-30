ed731cbeb9d4a492457a924f9830e9b5537f52d2
Richard Darwin Sy <rs106848@gmail.com>
2024-10-30 13:57:25 +0800

    .

--- Services/PersonalService.cs
+++ Services/PersonalService.cs
@@ -44,6 +44,48 @@ namespace Module07DataAccess.Services
                 }
             }
             return personalService;
-        }   
+        }
+        public async Task<bool> AddPersonalAsync(Personal newPerson)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    await conn.OpenAsync();
+                    var cmd = new MySqlCommand("INSERT INTO tblEmployee (Name, Address, email, ContactNo) VALUES (@Name, @Address, @Email, @ContactNo)", conn);
+                    cmd.Parameters.AddWithValue("@Name", newPerson.Name);
+                    cmd.Parameters.AddWithValue("@Address", newPerson.Address);
+                    cmd.Parameters.AddWithValue("@Email", newPerson.email);
+                    cmd.Parameters.AddWithValue("@ContactNo", newPerson.ContactNo);
+
+                    var result = await cmd.ExecuteNonQueryAsync();
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding Employee record: {ex.Message}");
+                return false;
+            }
+        }
+        public async Task<bool> DeletePersonalAsync(int EmployeeId)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    await conn.OpenAsync();
+                    var cmd = new MySqlCommand("DELETE FROM tblEmployee WHERE EmployeeId=@ID", conn);
+                    cmd.Parameters.AddWithValue("@ID", EmployeeId);
+                    var result = await cmd.ExecuteNonQueryAsync();
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Deleting Employee Record: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
--- ViewModel/PersonalViewModel.cs
+++ ViewModel/PersonalViewModel.cs
@@ -29,6 +29,39 @@ namespace Module07DataAccess.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        private Personal _selectedPersonal;
+        public Personal SelectedPersonal
+        {
+            get => _selectedPersonal;
+            set
+            {
+                _selectedPersonal = value;
+                if (_selectedPersonal != null)
+                {
+                    NewPersonalName = _selectedPersonal.Name;
+                    NewPersonalAddress = _selectedPersonal.Address;
+                    NewPersonalemail = _selectedPersonal.email;
+                    NewPersonalContactNo = _selectedPersonal.ContactNo;
+                    IsPersonSelected = true;
+                }
+                else
+                {
+                    IsPersonSelected = false;
+                }
+                OnPropertyChanged();
+            }
+        }
+        private bool _isPersonSelected;
+        public bool IsPersonSelected
+        {
+            get => _isPersonSelected;
+            set
+            {
+                _isPersonSelected = value;
+                OnPropertyChanged();
+            }
+        }
         private string _statusMessage;
         public string StatusMessage
         {
@@ -39,7 +72,56 @@ namespace Module07DataAccess.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        private string _newPersonalName;
+        public string NewPersonalName
+        {
+            get => _newPersonalName;
+            set
+            {
+                _newPersonalName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newPersonalemail;
+        public string NewPersonalemail
+        {
+            get => _newPersonalemail;
+            set
+            {
+                _newPersonalemail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newPersonalAddress;
+        public string NewPersonalAddress
+        {
+            get => _newPersonalAddress;
+            set
+            {
+                _newPersonalAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newPersonalContactNo;
+        public string NewPersonalContactNo
+        {
+            get => _newPersonalContactNo;
+            set
+            {
+                _newPersonalContactNo = value;
+                OnPropertyChanged();
+            }
+        }
         public ICommand LoadDataCommand { get; }
+        public ICommand AddPersonalCommand { get; }
+
+        public ICommand SelectedPersonCommand { get; }
+
+        public ICommand DeletePersonCommand { get; }
 
         //PersonlViewModel Constructor
 
@@ -48,6 +130,9 @@ namespace Module07DataAccess.ViewModel
             _personalService = new PersonalService();
             PersonalList = new ObservableCollection<Personal>();
             LoadDataCommand = new Command(async () => await LoadData());
+            AddPersonalCommand = new Command(async () => await AddEmployee());
+            SelectedPersonCommand = new Command<Personal>(person => SelectedPersonal = person);
+            DeletePersonCommand = new Command(async () => await DeletePersonal(), () => SelectedPersonal != null);
 
             LoadData();
 
@@ -66,7 +151,11 @@ namespace Module07DataAccess.ViewModel
 
                     var displayPersonal = new Personal
                     {
+                        EmployeeId = personal.EmployeeId,
                         Name = personal.Name,
+                        Address = personal.Address,
+                        email = personal.email,
+                        ContactNo = personal.ContactNo,
                         FullInfo = $"Address: {personal.Address}, Email: {personal.email}, Contact No: {personal.ContactNo}"
                     };
                     PersonalList.Add(displayPersonal);
@@ -82,6 +171,80 @@ namespace Module07DataAccess.ViewModel
                 IsBusy = false;
 
             }
+        }
+
+        private async Task AddEmployee()
+        {
+            if (IsBusy || string.IsNullOrWhiteSpace(NewPersonalName) || string.IsNullOrWhiteSpace(NewPersonalAddress) || string.IsNullOrWhiteSpace(NewPersonalemail) || string.IsNullOrWhiteSpace(NewPersonalContactNo))
+            {
+                StatusMessage = "Please Fill in all fields before adding";
+                return;
+            }
+            IsBusy = true;
+            StatusMessage = "Adding new Employee...";
+
+            try
+            {
+                var newPerson = new Personal
+                {
+                    Name = NewPersonalName,
+                    Address = NewPersonalAddress,
+                    email = NewPersonalemail,
+                    ContactNo = NewPersonalContactNo,
+                };
+                var isSuccess = await _personalService.AddPersonalAsync(newPerson);
+                if (isSuccess)
+                {
+                    NewPersonalName = string.Empty;
+                    NewPersonalAddress = string.Empty;
+                    NewPersonalemail = string.Empty;
+                    NewPersonalContactNo = string.Empty;
+                    StatusMessage = "New Employee added Successfully";
+                }
+                else
+                {
+                    StatusMessage = "Failed to add the new Employee";
+                };
+
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed adding Employee: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+                await LoadData();
+            }
+        }
+
+        private async Task DeletePersonal()
+        {
+            if (SelectedPersonal == null) return;
+            var answer = await Application.Current.MainPage.DisplayAlert("Confirm Delete", $"Are you sure you want to delete {SelectedPersonal.Name}?", "Yes", "No");
+            if (!answer) return;
+            IsBusy = true;
+            StatusMessage = "Deleting Employee";
+            try
+            {
+                var success = await _personalService.DeletePersonalAsync(SelectedPersonal.EmployeeId);
+                StatusMessage = success ? "Employee deleted successfully" : "Failed to delete Employee";
+                if (success)
+                {
+                    PersonalList.Remove(SelectedPersonal);
+                    SelectedPersonal = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error deleting Employee: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+                //await LoadData();
+            }
+
         }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
